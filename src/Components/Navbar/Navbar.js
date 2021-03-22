56e946e21c83744b0cd360f87db14792ab2658ae
Manne Vamsi <manne.vamsi55@ibm.com>
2021-03-22 11:30:26 +0530

    some changes

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -25,8 +25,6 @@ const Navbar = () => {
                                         <li className="nav-item">
                                         <Link className='nav-link' to='/signup'>Signup</Link>
                                     </li>
-                                   
-                                  
                                         <li className="nav-item">
                                         <Link className='nav-link' to='/login'>Login</Link>
                                     </li>                                   
